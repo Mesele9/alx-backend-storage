30c08c3c3db03290796468a1badc76354e56a32c
Mesele Assefa <getmesy@gmail.com>
2023-07-17 13:00:57 +0000

    correction

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,8 @@
 -- SQL script that creates a trigger that decreases the quantity
 -- of an item after adding a new order.
-
-USE holberton;
-
 DELIMITER //
+
+
 CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
@@ -11,4 +10,6 @@ BEGIN
 	SET quantity = quantity - NEW.number
 	WHERE name = NEW.item_name;
 END //
+
+
 DELIMITER ;
