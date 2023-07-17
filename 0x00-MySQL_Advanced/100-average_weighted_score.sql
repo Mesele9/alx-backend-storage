66f5793248b9d5b99dd9fdc6995eaee476d78410
Mesele Assefa <getmesy@gmail.com>
2023-07-17 16:24:00 +0000

    correction

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -4,23 +4,27 @@
 DELIMITER //
 
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
 BEGIN
 	DECLARE total_score FLOAT;
-	DECLARE total_weight FLOAT;
+	DECLARE total_weight INT;
 	DECLARE average_score FLOAT;
 
 	SELECT SUM(c.score * p.weight), SUM(p.weight)
 	INTO total_score, total_weight
 	FROM corrections c
-	JOIN projects p ON c.project_id = p.id
+	INNER JOIN projects p ON c.project_id = p.id
 	WHERE c.user_id = user_id;
 
-	SET average_score = IF(total_weight > 0, total_score / total_weight, 0);
+	IF total_weight > 0 THEN
+		SET average_score = total_score / total_weight;
+	ELSE 
+		SET average_score = 0;
+	END IF;
 
 	UPDATE users
 	SET average_score = average_score
-	WHERE id - user_id;
+	WHERE id = user_id;
 END //
 
 
