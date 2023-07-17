c9d95dea0170853679104d36b88fa1b77633b1ef
Mesele Assefa <getmesy@gmail.com>
2023-07-17 14:30:03 +0000

    correction

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,13 +5,25 @@ DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE avg_score FLOAT;
+	DECLARE average_score FLOAT;
+	DECLARE num_corrections INT;
+	DECLARE total_score FLOAT;
+
+	-- Compute total score for a user
+	SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = user_id;
+
+	-- compute the number of corrections
+	SELECT COUNT(*) INTO num_corrections FROM corrections WHERE user_id = user_id;
 
 	-- Compute average score
-	SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+	IF num_corrections > 0 THEN
+		SET average_score = total_score / num_corrections;
+	ELSE 
+		SET average_score = 0;
+	END IF;
 
 	-- update the average_score column for theuser
-	UPDATE users SET average_score = avg_score WHERE id = user_id;
+	UPDATE users SET average_score = average_score WHERE id = user_id;
 END //
 
 
