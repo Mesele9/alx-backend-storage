6f59b63f0922c186e22ffade933c91be2052c4d7
Mesele Assefa <getmesy@gmail.com>
2023-07-19 04:54:15 +0300

    correction

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,5 +4,5 @@
 
 def list_all(mongo_collection):
     if mongo_collection.count == 0:
-        return
-    return list(mongo_collection.find())
+        return {}
+    return list(mongo_collection.find({}))
