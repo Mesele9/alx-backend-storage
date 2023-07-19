d539cd0944169900431ff2ffcedce5468fb96a7c
Mesele Assefa <getmesy@gmail.com>
2023-07-19 05:37:28 +0300

    correction

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""a Python function that lists all documents in a collection"""
+""" List all documents in Python """
 
 
 def list_all(mongo_collection):
+    """ a Python function that lists all documents in a collection"""
     if mongo_collection.count() == 0:
         return []
     return list(mongo_collection.find({}))
+
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,6 +4,7 @@ based on kwargs: """
 
 
 def insert_school(mongo_collection, **kwargs):
+    """ Python function that inserts a new document in a collection based on kwargs"""
     res = mongo_collection.insert_one(kwargs)
     return res.inserted_id
 
